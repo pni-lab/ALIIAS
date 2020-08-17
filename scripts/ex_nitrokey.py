4e4a81f51119d815b65cb2736bc182b557c7f597
Tamas Spisak <spisak.tms@gmail.com>
2020-08-17 17:52:17 +0200

    comments added

--- scripts/ex_nitrokey.py
+++ scripts/ex_nitrokey.py
@@ -13,13 +13,17 @@ lib = pkcs11.lib('/usr//local/lib/opensc-pkcs11.so')
 #token = lib.get_token(token_label="Nitrokey (UserPIN)")
 token = lib.get_token()
 
-data = b'INPUT DATA'
-
 # Open a session on our token
+# token was initialized with:
 #sc-hsm-tool --initialize --so-pin 3537363231383830 --pin 648219 --label "Nitrokey"
+# In case of the "pkcs11.exceptions.MultipleObjectsReturned: More than 1 key matches"
+# re-initialize the token with sc-hsm-tool
+#
 with token.open(user_pin='648219', rw=True) as session:
     # Generate an RSA keypair in this session
-    #pub, priv = session.generate_keypair(pkcs11.KeyType.RSA, 2048)
+    # this only has to be done once, and shouldn't be part of PseudoID (only as a utility to set up keys)
+    # comment this out if key was already generated, or re-initialize (see above)
+    pub, priv = session.generate_keypair(pkcs11.KeyType.RSA, 2048)
 
     # Extract public key
     key = session.get_key(key_type=KeyType.RSA,
