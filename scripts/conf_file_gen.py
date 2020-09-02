6f0d9aca721296b2c3958d6951c579cdf0c61f6d
renglert <robertenglert92@gmail.com>
2020-09-02 13:54:00 +0200

    updated conf_file_gen.py

--- scripts/conf_file_gen.py
+++ scripts/conf_file_gen.py
@@ -1,19 +1,35 @@
 import configparser
+
 # sc: https://linuxhint.com/read_write_ini_conf_python/
 
 config = configparser.ConfigParser()
 
-config['DEFAULT'] = {"Log Level": 10}
+_exp_tag_ = {
+    "": "",
+    "Pre": "-pre",
+    "Post": "-post",
+    "Baseline": "-bsl",
+    "Week 1": "-wk1",
+    "Week 2": "-wk2",
+    "Week 3": "-wk3",
+    "Week 4": "-wk4",
+}
+
+config['BASE'] = {"Log_level": 10,
+                  "exp_tag": _exp_tag_}
 
 config['PSEUDOKEYS'] = {}
 
 config['ENCRYPTION'] = {"Type": "AES", "short_id_length": "8"}
 
-config['LIMESURVEY'] = {}
+_ls_url_base_ = 'https://www.uni-due.de/~ht2203/limesurvey'
+
+config['LIMESURVEY'] = {"active": True,
+                        "url_base": _ls_url_base_,
+                        "url_rc": _ls_url_base_ + '/index.php/admin/remotecontrol',
+                        "url_login": _ls_url_base_ + '/index.php/admin/authentication/sa/login'}
 
 config['BARCODES'] = {"n_diff_bc": 6, "n_identical_bc": 3}
 
-
 with open('../pseudoID/settings.conf', 'w') as configfile:
     config.write(configfile)
-
