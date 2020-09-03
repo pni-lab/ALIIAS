9d50c1561b0f3dafaec1d72832bb97bc0ae2a9c1
Tamas Spisak <spisak.tms@gmail.com>
2020-09-03 12:27:55 +0200

    LimeSurvey API extended

--- pseudoID/ls_api_wrapper.py
+++ pseudoID/ls_api_wrapper.py
@@ -25,6 +25,53 @@ class LimeSurveyController:
 
         return ret
 
+    def get_participants(self, survey_id):
+        # list_participants(string $sSessionKey,integer $iSurveyID,integer $iStart,integer $iLimit = 10,
+        # boolean $bUnused = false,boolean|array $aAttributes = false,array $aConditions = array()): array
+
+        params = OrderedDict([
+            ('sSessionKey', self.session_key),
+            ('iSurveyID', survey_id),
+            ('iStart', 0),
+            ('iLimit', 1000000000),
+            ('aAttributes', 'true')
+        ])
+
+        data = self.api.utils.prepare_params('list_participants', params)
+        response = self.api.utils.request(data)
+        return response['result']
+
+    def contains_participant(self, survey_id, short_id, long_id):
+
+        ret = self.get_participants(survey_id=survey_id)
+
+        for part in ret:
+            if part['participant_info']['firstname'] == short_id:
+                if part['participant_info']['lastname'] != long_id:
+                    # long id's not the same
+                    raise Exception("Possible Duplicate Detected!")
+                else:
+                    return True
+            else:
+                return False
+
+    def register_to_survey(self, short_id, long_id, survey_id):
+        #string $sSessionKey, integer $iSurveyID, array $aParticipantData, boolean $bCreateToken = true): array
+        participant_data = [{"firstname": short_id,
+                             "lastname": long_id,
+                             "language": "de"
+                             }]
+
+        params = OrderedDict([
+            ('sSessionKey', self.session_key),
+            ('iSurveyID', survey_id),
+            ('aParticipants', participant_data)
+        ])
+
+        data = self.api.utils.prepare_params('add_participants', params)
+        response = self.api.utils.request(data)
+        return response
+
     def register_in_cpdb(self, short_id, long_id, logical_delete=False):
 
         if logical_delete:
@@ -47,5 +94,13 @@ class LimeSurveyController:
         data = self.api.utils.prepare_params('cpd_importParticipants', params)
         response = self.api.utils.request(data)
         return response
+        # todo when cloning survey: init participant table, link longID with a dummy participant
 
-# todo when cloning survey: init participant table, link longID with a dummy participant
+    def close_session(self):
+        #release_session_key(string $sSessionKey): string
+        params = OrderedDict([
+            ('sSessionKey', self.session_key)
+        ])
+        data = self.api.utils.prepare_params('release_session_key', params)
+        response = self.api.utils.request(data)
+        return response
