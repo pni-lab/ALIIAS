7bcad1d8382937fb2118b0e8bc36914eed064359
RobertEnglert <robertenglert92@gmail.com>
2020-09-11 12:07:34 +0200

    moved barcode params to config and added one png which contains all barcodes

--- examples/barcode_ex.py
+++ examples/barcode_ex.py
@@ -4,16 +4,16 @@ import PIL
 from PIL import Image
 
 writer = ImageWriter()
-barcode = barcode.get('code128', 'f2d5h60a', writer=writer)
+barcode = barcode.get('code128', '32d5h60a', writer=writer)
 filename = barcode.save('barcode_test',
                         {"module_width": 38.1/110,
                          "module_height": 19.05,
                          "text_distance": 1.0,
-                         "font_size": 16})
+                         "font_size": 40})
 
 
 to_be_resized = Image.open(filename)
 newSize = (750, 375)  # for 38.1mmx19.05mm at 500dpi
 resized = to_be_resized.resize(newSize, resample=PIL.Image.BICUBIC)
 
-resized.save('barcode_test.png')
+resized.save('barcode_test3.png')
